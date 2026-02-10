0b28bdade9a868389deb1a8c2101d1bd5ac435ed
Awet Thon <awetthon8@gmail.com>
2026-02-10 15:07:28 +0200

    use api response in user service

--- backend/services/user_service.py
+++ backend/services/user_service.py
@@ -1,28 +1,47 @@
 from schemas.user_schema import UserSchema
 from db.core import get_session
 from db.models import User
+from utils.api_response import APIResponse
 
 
 class UserService:
     @classmethod
-    def create_user(cls, user:UserSchema):
+    def create_user(cls, user: UserSchema) -> APIResponse:
         with get_session() as db:
             db_user = User(**user.dict())
             db.add(db_user)
             db.commit()
-            return db_user
-    
-    
+            response = APIResponse()
+
+            if db_user:
+                response.success = True
+                response.message = "user created successfully"
+                response.data = [db_user]
+            else:
+                response.message = "failed to create user"
+
+            return response
+
     @classmethod
-    def update_user(cls, id, user):
+    def update_user(cls, id, user) -> APIResponse:
         pass
-    
+
     @classmethod
-    def get_user_by_id(cls, id):
+    def get_user_by_id(cls, id) -> APIResponse:
         pass
-    
+
     @classmethod
-    def get_user_all(cls):
+    def get_user_all(cls) -> APIResponse:
         with get_session() as db:
             users = db.query(User).all()
-            return users or []
+
+            response = APIResponse()
+
+            if users:
+                response.success = True
+                response.message = "found users"
+                response.data = users
+            else:
+                response.message = "could not find users"
+
+            return response
