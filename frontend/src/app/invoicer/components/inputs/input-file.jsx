c00333c0cbae4e1bc84115f23785de61a876480c
Awet Thon <awetthon8@gmail.com>
2026-02-12 15:46:43 +0200

    file input

---
+++ frontend/src/app/invoicer/components/inputs/input-file.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const InputFile = ({
+  label = "Label",
+  id,
+  name,
+  value,
+  placeholder,
+  error_message,
+  onChange,
+  hint = "",
+}) => {
+  return (
+    <>
+      <fieldset className="fieldset">
+        <legend className="mb-1 md:mb-2 block text-sm md:text-base md:font-medium text-gray-600">
+          {label}
+        </legend>
+        <input
+          type="file"
+          className="input-file-primary w-full rounded-md border border-[#e0e0e0]  py-1 px-2 text-sm placeholder:text-sm md:py-2 md:px-4 md:text-base md:font-medium text-[#6B7280] outline-none  focus:border-teal-600 focus:shadow-md"
+          name={name}
+          value={value}
+        />
+        <label className="text-gray-400 text-xs">Max size 2MB</label>
+      </fieldset>
+    </>
+  );
+};
+
+export default InputFile;
--- frontend/src/app/invoicer/invoices/components/invoice-form.jsx
+++ frontend/src/app/invoicer/invoices/components/invoice-form.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import InputText from "../../components/inputs/input-text";
 import InputAmount from "../../components/inputs/input-amount";
+import InputFile from "../../components/inputs/input-file";
 
 const InvoiceForm = () => {
   return (
@@ -17,6 +18,8 @@ const InvoiceForm = () => {
       <br />
 
       <InputAmount label={"Amount "} placeholder={"e.g 10000"} />
+      <br />
+      <InputFile label="Attachment" />
 
       <div className="flex justify-end mt-4 gap-2 ">
         <button className="btn btn-sm btn-soft rounded">Cancel</button>
