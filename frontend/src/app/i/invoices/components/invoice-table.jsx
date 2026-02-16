2b661bc81ab0ef3f3b2ba80c7e24a12c30e6ad07
Awet Thon <awetthon8@gmail.com>
2026-02-16 11:25:19 +0200

    refactor

--- frontend/src/app/i/invoices/components/invoice-table.jsx
+++ frontend/src/app/i/invoices/components/invoice-table.jsx
@@ -98,9 +98,7 @@ const InvoiceTable = ({ data = [] }) => {
                 id="filterDropdown"
                 className="z-10 hidden w-48 p-3 bg-white rounded-lg shadow dark:bg-teal-600"
               >
-                <h6 className="mb-3 text-sm font-medium text-white dark:text-white">
-                  Status
-                </h6>
+                <h6 className="mb-3 text-sm font-medium text-white ">Status</h6>
                 <ul
                   className="space-y-2 text-sm"
                   aria-labelledby="filterDropdownButton"
@@ -171,103 +169,7 @@ const InvoiceTable = ({ data = [] }) => {
           </table>
         </div>
 
-        {/* pagination */}
-        {/* <nav
-        className="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4"
-        aria-label="Table navigation"
-      >
-        <span className="text-sm font-normal text-gray-500 ">
-          Showing
-          <span className="font-semibold text-white dark:text-white">1-10</span>
-          of
-          <span className="font-semibold text-white dark:text-white">1000</span>
-        </span>
-        <ul className="inline-flex items-stretch -space-x-px">
-          <li>
-            <a
-              href="#"
-              className="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-white dark:bg-teal-600 dark:border-gray-700  dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              <span className="sr-only">Previous</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                fill="currentColor"
-                viewbox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-white dark:bg-teal-600 dark:border-gray-700  dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              1
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-white dark:bg-teal-600 dark:border-gray-700  dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              2
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              aria-current="page"
-              className="flex items-center justify-center text-sm z-10 py-2 px-3 leading-tight text-teal-600 bg-teal-50 border border-teal-300 hover:bg-teal-100 hover:text-teal-700 dark:border-gray-700 dark:bg-teal-600 dark:text-white"
-            >
-              3
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-white dark:bg-teal-600 dark:border-gray-700  dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              ...
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-white dark:bg-teal-600 dark:border-gray-700  dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              100
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-white dark:bg-teal-600 dark:border-gray-700  dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              <span className="sr-only">Next</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                fill="currentColor"
-                viewbox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </a>
-          </li>
-        </ul>
-      </nav> */}
+        {/* pagination will go here */}
       </div>
     </div>
   );
