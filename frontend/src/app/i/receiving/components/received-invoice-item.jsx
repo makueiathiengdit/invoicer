d0da6c613f2668cc154c0fb0e38036f36ff8f0fc
Awet Thon <awetthon8@gmail.com>
2026-02-23 10:48:00 +0200

    update received item

--- frontend/src/app/i/receiving/components/received-invoice-item.jsx
+++ frontend/src/app/i/receiving/components/received-invoice-item.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ReceivedInvoiceItem = ({ item = {} }) => {
   return (
-    <div className="flex gap-2 border border-gray-300 rounded p-4 mt-1">
+    <div className="flex justify-between gap-2 border border-gray-300 rounded p-4 mt-1">
       <div>
         <div className="flex gap-2 text-gray-700 font-semibold">
           <span>PR : {item?.pr_number || "N/A"}</span> |
@@ -12,9 +12,12 @@ const ReceivedInvoiceItem = ({ item = {} }) => {
           <span className="">
             Submited: {new Date(item.created_at).toDateString()}
           </span>{" "}
-          ||
-          <span>{item.description}</span>
+          -<span>{item.description}</span>
         </div>
+      </div>
+      <div className="flex gap-2 text-gray-700 ">
+        <span>{item.currency}</span>
+        <span className="font-semibold">{item.amount}</span>
       </div>
     </div>
   );
