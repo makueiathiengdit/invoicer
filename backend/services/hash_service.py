47b3c2bdbb0ad5f12dd08ab5721f6d169753cf85
Awet Thon <awetthon8@gmail.com>
2026-02-25 08:51:17 +0200

    password hash service

---
+++ backend/services/hash_service.py
@@ -0,0 +1,8 @@
+class HashService:
+    @classmethod
+    def hash(cls, password: str) -> str:
+        pass
+
+    @classmethod
+    def verify(cls, password: str, hash: str) -> bool:
+        pass
