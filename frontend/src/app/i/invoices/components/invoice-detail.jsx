326fddc4754df6fe3379c62c8321015212b17cc4
Awet Thon <awetthon8@gmail.com>
2026-03-03 12:06:09 +0200

    add invoice timeline

--- frontend/src/app/i/invoices/components/invoice-detail.jsx
+++ frontend/src/app/i/invoices/components/invoice-detail.jsx
@@ -3,6 +3,7 @@
 import { formatCurrentDate, getInvoiceStatusColor } from "@/app/utils/utils";
 import { Calendar, User, Hash, Paperclip, BadgeCheck } from "lucide-react";
 import UpdatePRPOForm from "./update-prpo-form";
+import InvoiceTimeline from "./invoice-timeline";
 
 export const sampleInvoice = {
   id: 1,
@@ -35,6 +36,15 @@ export default function InvoiceDetail({ invoice = sampleInvoice }) {
     ? invoice.assigned_user.first_name + " " + invoice.assigned_user.last_name
     : "Not assigned";
 
+  // timeline
+  const timeline = {
+    created_at: invoice.created_at,
+    pr_date: invoice.pr_date || new Date(),
+    po_date: invoice.po_date || new Date(),
+    received_date: "",
+    completed_at: "",
+  };
+
   return (
     <div className="min-h-screen print:bg-white print:p-0 p-6">
       <div className="flex  gap-3 justify-end mb-2 max-w-4xl mx-auto">
@@ -53,113 +63,112 @@ export default function InvoiceDetail({ invoice = sampleInvoice }) {
         </span>
       </div>
 
-      <div className="max-w-4xl mx-auto bg-white shadow-xl print:shadow-none rounded-2xl print:rounded-none  print:border-none">
-        <div className="bg-teal-600 text-white print:bg-white print:text-black p-6  print:border-gray-300">
-          <div className="flex justify-between items-start">
-            <div>
-              <h1 className="text-3xl font-bold tracking-tight">INVOICE</h1>
-              <p className="bg-amber-50 text-gray-700 px-1 font-mono text-sm mt-1 opacity-90 print:opacity-100">
-                Invoice ID: {invoice.invoice_id || "N/A"}
-              </p>
+      <div className="max-w-4xl mx-auto flex gap-5 bg-white shadow-xl print:shadow-none rounded-2xl print:rounded-none  print:border-none">
+        <div>
+          <div className="bg-teal-600 text-white print:bg-white print:text-black p-6  print:border-gray-300">
+            <div className="flex justify-between items-start">
+              <div>
+                <h1 className="text-3xl font-bold tracking-tight">INVOICE</h1>
+                <p className="bg-amber-50 text-gray-700 px-1 font-mono text-sm mt-1 opacity-90 print:opacity-100">
+                  Invoice ID: {invoice.invoice_id || "N/A"}
+                </p>
+              </div>
+
+              <div className="text-right">
+                <p className="text-sm uppercase tracking-wide">Total Amount</p>
+                <p className="text-3xl font-extrabold">
+                  {formatCurrency(invoice.amount)}
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-8 space-y-8">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-sm">
+              <InfoItem
+                icon={<Calendar size={18} />}
+                label="Date"
+                value={formatCurrentDate(new Date(invoice.created_at))}
+              />
+
+              <InfoItem
+                icon={<User size={18} />}
+                label="Vendor"
+                value={invoice.vendor || "Not specified"}
+              />
+
+              <InfoItem
+                icon={<Hash size={18} />}
+                label="PR Number"
+                value={invoice.pr_number || "—"}
+              />
+
+              <InfoItem
+                icon={<Hash size={18} />}
+                label="PO Number"
+                value={invoice.po_number || "—"}
+              />
+
+              <InfoItem
+                icon={<User size={18} />}
+                label="Assigned to"
+                value={assigned_user}
+              />
+              <InfoItem
+                icon={<BadgeCheck size={18} />}
+                label="Status"
+                value={invoice.status || "Unknown"}
+                text_color={getInvoiceStatusColor(invoice.status)}
+              />
             </div>
 
-            <div className="text-right">
-              <p className="text-sm uppercase tracking-wide">Total Amount</p>
-              <p className="text-3xl font-extrabold">
-                {formatCurrency(invoice.amount)}
-              </p>
+            <div>
+              <h3 className="text-sm font-semibold uppercase tracking-wide text-gray-500 mb-3">
+                Description
+              </h3>
+              <div className="border border-gray-200 rounded-xl p-5 leading-relaxed text-gray-700">
+                {invoice.description || "No description provided."}
+              </div>
             </div>
+
+            {invoice.attachment && (
+              <div>
+                <h3 className="text-sm font-semibold uppercase tracking-wide text-gray-500 mb-3">
+                  Attachment
+                </h3>
+                <div className="flex justify-between gap-3 border border-gray-200 rounded-xl p-4">
+                  <div className="flex justify-center gap-3">
+                    <Paperclip
+                      size={18}
+                      className="text-teal-600 print:text-black"
+                    />
+                    <span>{invoice.attachment.name}</span>
+
+                    <span className="text-gray-400 ">
+                      {(invoice.attachment.size / (1024 * 1024)).toFixed(1)}MB
+                    </span>
+                  </div>
+
+                  <div className="flex justify-end items-end">
+                    <a
+                      href={`/i/files/${invoice?.attachment.id}`}
+                      target="_blank"
+                      className="btn btn-primary btn-sm rounded-md text-white"
+                    >
+                      View
+                    </a>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </div>
 
-        <div className="p-8 space-y-8">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-sm">
-            <InfoItem
-              icon={<Calendar size={18} />}
-              label="Date"
-              value={formatCurrentDate(new Date(invoice.created_at))}
-            />
+        {/* timeline */}
+        <div>
+          {/* progress timeline from date invoice was created to date ivoice pr was created to po to receiving date to date it is finally completed */}
 
-            <InfoItem
-              icon={<User size={18} />}
-              label="Vendor"
-              value={invoice.vendor || "Not specified"}
-            />
-
-            <InfoItem
-              icon={<Hash size={18} />}
-              label="PR Number"
-              value={invoice.pr_number || "—"}
-            />
-
-            <InfoItem
-              icon={<Hash size={18} />}
-              label="PO Number"
-              value={invoice.po_number || "—"}
-            />
-
-            <InfoItem
-              icon={<User size={18} />}
-              label="Assigned to"
-              value={assigned_user}
-            />
-            <InfoItem
-              icon={<BadgeCheck size={18} />}
-              label="Status"
-              value={invoice.status || "Unknown"}
-              text_color={getInvoiceStatusColor(invoice.status)}
-            />
-          </div>
-
-          <div>
-            <h3 className="text-sm font-semibold uppercase tracking-wide text-gray-500 mb-3">
-              Description
-            </h3>
-            <div className="border border-gray-200 rounded-xl p-5 leading-relaxed text-gray-700">
-              {invoice.description || "No description provided."}
-            </div>
-          </div>
-
-          {invoice.attachment && (
-            <div>
-              <h3 className="text-sm font-semibold uppercase tracking-wide text-gray-500 mb-3">
-                Attachment
-              </h3>
-              <div className="flex justify-between gap-3 border border-gray-200 rounded-xl p-4">
-                <div className="flex justify-center gap-3">
-                  <Paperclip
-                    size={18}
-                    className="text-teal-600 print:text-black"
-                  />
-                  <span>{invoice.attachment.name}</span>
-
-                  <span className="text-gray-400 ">
-                    {(invoice.attachment.size / (1024 * 1024)).toFixed(1)}MB
-                  </span>
-                </div>
-
-                <div className="flex justify-end items-end">
-                  <a
-                    href={`/i/files/${invoice?.attachment.id}`}
-                    target="_blank"
-                    className="btn btn-primary btn-sm rounded-md text-white"
-                  >
-                    View
-                  </a>
-                </div>
-              </div>
-            </div>
-          )}
-
-          <div>
-            {/* progress timeline from date invoice was created to date ivoice pr was created to po to receiving date to date it is finally completed */}
-            <h3 className="text-sm font-semibold uppercase tracking-wide text-gray-500 mb-3">
-              Timeline
-            </h3>
-            <div className="border border-gray-200 rounded-xl p-5 leading-relaxed text-gray-700 h-18">
-              {/* {invoice.description || "No description provided."} */}
-            </div>
-          </div>
+          <InvoiceTimeline timeline={timeline} />
         </div>
       </div>
 
