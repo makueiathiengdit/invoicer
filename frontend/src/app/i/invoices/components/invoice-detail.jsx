34b475b3650d1df67c5447e265428b06460112aa
Awet Thon <awetthon8@gmail.com>
2026-03-05 11:03:45 +0200

    update

--- frontend/src/app/i/invoices/components/invoice-detail.jsx
+++ frontend/src/app/i/invoices/components/invoice-detail.jsx
@@ -43,8 +43,8 @@ export default function InvoiceDetail({ invoice = sampleInvoice }) {
     created_at: invoice.created_at,
     pr_date: invoice.pr_date,
     po_date: invoice.po_date,
-    received_date: "",
-    completed_at: "",
+    received_date: invoice?.completed_date,
+    completed_at: invoice?.completed_date,
   };
 
   // auto fetch received invoices
