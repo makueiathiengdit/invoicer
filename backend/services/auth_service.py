a058ee352a2068cc879636cad4814a88f916d394
Awet Thon <awetthon8@gmail.com>
2026-03-16 10:48:54 +0200

    auth service

---
+++ backend/services/auth_service.py
@@ -0,0 +1,11 @@
+from utils.api_response import APIResponse
+
+
+class AuthService:
+    @classmethod
+    def login(cls, user) -> APIResponse:
+        pass
+
+    @classmethod
+    def logout(cls) -> APIResponse:
+        pass
