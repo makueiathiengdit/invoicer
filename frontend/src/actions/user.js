78d060e7a0c56e515b121c2af34110953bccc049
Awet Thon <awetthon8@gmail.com>
2026-03-22 21:40:47 +0200

    update

--- frontend/src/actions/user.js
+++ frontend/src/actions/user.js
@@ -4,4 +4,4 @@ export async function createUser(user) {}
 export async function updateUser(id, user_data) {}
 export async function getUserByID(id) {}
 export async function getUserByEmail(email) {}
-export async function getUsersAll(params) {}
+export async function getUsersAll(filter) {}
