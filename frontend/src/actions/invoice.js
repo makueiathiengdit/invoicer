1ef43813eed77dbbd9b4a7f16644f5f7b2e84d12
Awet Thon <awetthon8@gmail.com>
2026-03-23 08:43:16 +0200

    invoice actions

--- frontend/src/actions/invoice.js
+++ frontend/src/actions/invoice.js
@@ -1,6 +1,67 @@
 "use server";
 
-export async function createInvoice(params) {}
+import { ServerActionResponse } from "@/app/utils/server-action-response";
+import { connectToDB } from "@/db/connect";
+import { Invoice } from "@/db/models";
+
+export async function createInvoice(invoice) {
+  let response = new ServerActionResponse();
+
+  try {
+    await connectToDB();
+
+    db_inv = await Invoice.create(invoice);
+
+    if (db_inv) {
+      response.success = true;
+      response.message = "invoice created successfully";
+    } else {
+      response.message = "failed to create invoice";
+    }
+  } catch (error) {
+    response.message = "something went wrong (500)";
+  }
+
+  return JSON.stringify(response);
+}
 export async function updateInvoice(id, invoice_data) {}
-export async function getInvoiceByID(id) {}
-export async function getInvoiceAll(params) {}
+export async function getInvoiceByID(id) {
+  let response = new ServerActionResponse();
+
+  try {
+    await connectToDB();
+
+    db_invoices = await Invoice.findById(id);
+    if (db_invoices) {
+      response.success = true;
+      response.message = "found invoice";
+      response.data = [db_invoices];
+    } else {
+      response.message = "no invoice found";
+    }
+  } catch (error) {
+    response.message = "something went wrong (500)";
+  }
+
+  return JSON.stringify(response);
+}
+export async function getInvoiceAll(filter = {}) {
+  let response = new ServerActionResponse();
+
+  try {
+    await connectToDB();
+
+    db_invoices = await Invoice.find(filter);
+    if (db_invoices && db_invoices.length > 0) {
+      response.success = true;
+      response.message = "found invoices";
+      response.data = db_invoices;
+    } else {
+      response.message = "no invoices found";
+    }
+  } catch (error) {
+    response.message = "something went wrong (500)";
+  }
+
+  return JSON.stringify(response);
+}
