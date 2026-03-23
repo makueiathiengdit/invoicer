b0aa40561e98535af8e38fc30d8f4d31f5d6b098
Awet Thon <awetthon8@gmail.com>
2026-03-23 16:54:23 +0200

    update

--- frontend/src/app/i/receiving/components/received-invoice-wrapper.jsx
+++ frontend/src/app/i/receiving/components/received-invoice-wrapper.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 import ReceivedInvoiceList from "./received-invoice-list";
-import { delayRequest } from "@/app/utils/utils";
-import { BASE_API_URL } from "@/app/constants/constants";
 import { getReceivedInvoicesAll } from "@/actions/received-invoices";
 
 const ReceivedInvoiceWrapper = async () => {
   let received_invoices = [];
 
   try {
-    // const url = BASE_API_URL + "/received/invoices";
-
-    // let res = await fetch(url);
-    // res = await res.json();
-
     let res = await getReceivedInvoicesAll();
     res = JSON.parse(res);
     if (res._success) {
